9ea2e2e77fe75f0a74e4e712cdd3c1a6ba613f30
Oussama Lamnaouer <lmnouer59@gmail.com>
2023-01-22 14:54:26 +0100

    implement github auth

---
+++ src/components/AuthShowcase.tsx
@@ -0,0 +1,37 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const AuthShowcase = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="hidden items-center justify-end sm:flex md:flex-1 lg:w-0">
+      {sessionData ? (
+        <>
+          <button
+            onClick={() => void signOut()}
+            className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Sign out
+          </button>
+          <Link
+            href="/create"
+            className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+          >
+            Create
+          </Link>
+        </>
+      ) : (
+        <button
+          className=" inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+          onClick={() => void signIn()}
+        >
+          Sign in
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default AuthShowcase;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logo.png";
+import AuthShowcase from "./AuthShowcase";
 const Header = () => {
   return (
     <header className="align-center flex w-full justify-between border-b border-b-[#e6ebf4] bg-white px-4 py-4 sm:px-8">
       <Link href={"/"}>
         <Image src={logo} alt="OpenAI" className="w-28 object-contain" />
       </Link>
-      <Link
+      {/* <Link
         href={"/create"}
         className="rounded-md bg-[#6469ff] px-4 py-2 font-inter font-medium text-white "
       >
         Create
-      </Link>
+      </Link> */}
+      <AuthShowcase />
     </header>
   );
 };
